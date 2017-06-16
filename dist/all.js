cfb572f0f692681a679c6b628620f444373a5aa7
Luiz Felipe <frazaolipe@gmail.com>
2017-06-16 17:35:14 -0300

    [1] Criação do arquivo.

---
+++ dist/all.js
@@ -0,0 +1,27 @@
+/*
+ *! all.js
+ *
+ * Todos os arquivos do projeto serão compilados aqui.
+ * Copyright (c) 2017. Todos os direitos reservados.
+ *
+ * MIT Licence.
+ */
+
+(function ($) {
+  'use strict';
+
+  $(function () {
+    // Script com todas as funções da página do Instalador:
+    $.getScript('page.js');
+    
+    // Script com todos os tutoriais:
+    $.getScript('tutorials.js');
+
+    // Script com as funções AJAX do ACP:
+    $.getScript('ajax.js');
+  });
+
+  $(window).on('load', function () {
+    console.log('[Instalador] Página carregada!');
+  });
+}(jQuery));
