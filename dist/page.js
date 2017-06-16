e9211de1d875bc73762a32f0e4b4a058ee0a36d7
Luiz Felipe <frazaolipe@gmail.com>
2017-06-16 17:36:04 -0300

    [1] Criação do arquivo.

---
+++ dist/page.js
@@ -0,0 +1,20 @@
+/*
+ *! all.js
+ *
+ * Todos os arquivos do projeto serão compilados aqui.
+ * Copyright (c) 2017. Todos os direitos reservados.
+ *
+ * MIT Licence.
+ */
+
+(function ($) {
+  'use strict';
+
+  $(function () {
+    // Funções da página.
+  });
+
+  $(window).on('load', function () {
+    console.log('[Instalador] Página carregada! [page.js]');
+  });
+}(jQuery));
