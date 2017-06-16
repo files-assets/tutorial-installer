400e22d16e8d903f23f756f71e50ce96da064348
Luiz Felipe <frazaolipe@gmail.com>
2017-06-16 17:37:27 -0300

    [1] Criação do arquivo.

---
+++ dist/ajax.js
@@ -0,0 +1,20 @@
+/*
+ *! ajax.js
+ *
+ * Todos os arquivos AJAX do Instalador encontram-se aqui.
+ * Copyright (c) 2017. Todos os direitos reservados.
+ *
+ * MIT Licence.
+ */
+
+(function ($) {
+  'use strict';
+
+  $(function () {
+    // Funções ajax.
+  });
+
+  $(window).on('load', function () {
+    console.log('[Instalador] Página carregada! [ajax.js]');
+  });
+}(jQuery));
