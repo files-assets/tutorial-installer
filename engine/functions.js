f342aa0c05a571ae37abe5ddac60fe42a5d06bfb
Luiz Felipe <lffg@users.noreply.github.com>
2017-06-25 20:23:18 -0300

    Edição e correção.

--- engine/functions.js
+++ engine/functions.js
@@ -1,36 +1,34 @@
 /*
- *! functions.js
+ *! page.js
  *
- * Funções gerais para o instalador (como o log), encontram-se aqui.
+ * Todos os arquivos do Instalador (página em si) encontram-se aqui.
  * Copyright (c) 2017. Todos os direitos reservados.
  *
  * MIT Licence.
  */
 
-(function ($) {
-  'use strict';
+'use strict';
 
-  var install = {
-    log: function (log) {
-      $('.install-log ul').append('<li class="install-log">' + log + '</li>');
-    },
-    
-    sucess: function (log) {
-      $('.install-log ul').append('<li class="install-log log-sucess">' + log + '</li>');
-    },
-    
-    warn: function (log) {
-      $('.install-log ul').append('<li class="install-log log-warn">' + log + '</li>');
-    },
-    
-    error: function (log) {
-      $('.install-log ul').append('<li class="install-log log-error">' + log + '</li>');
-    }
-  };
+var install = {
+  log: function (log) {
+    $('.install-log ul').append('<li class="install-log">' + log + '</li>');
+  },
+  
+  sucess: function (log) {
+    $('.install-log ul').append('<li class="install-log log-sucess">' + log + '</li>');
+  },
+  
+  warn: function (log) {
+    $('.install-log ul').append('<li class="install-log log-warn">' + log + '</li>');
+  },
+  
+  error: function (log) {
+    $('.install-log ul').append('<li class="install-log log-error">' + log + '</li>');
+  }
+};
 
-  $(window).on('load', function () {
-    install.sucess('A página JavaScript principal foi carregada com sucesso!');
-    install.sucess('A página JavaScript das funções gerais foi carregada com sucesso!');
-    console.log('[Instalador] Página carregada! [functions.js]');
-  });
-}(jQuery));
+$(window).on('load', function () {
+  install.sucess('A página JavaScript principal foi carregada com sucesso!');
+  install.sucess('A página JavaScript das funções gerais foi carregada com sucesso!');
+  console.log('[Instalador] Página carregada! [functions.js]');
+});
