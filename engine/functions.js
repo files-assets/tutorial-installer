f60e254067edd17224351e129274272d3f82e882
Luiz Felipe <lffg@users.noreply.github.com>
2017-06-25 20:15:41 -0300

    Criação do arquivo.

---
+++ engine/functions.js
@@ -0,0 +1,36 @@
+/*
+ *! functions.js
+ *
+ * Funções gerais para o instalador (como o log), encontram-se aqui.
+ * Copyright (c) 2017. Todos os direitos reservados.
+ *
+ * MIT Licence.
+ */
+
+(function ($) {
+  'use strict';
+
+  var install = {
+    log: function (log) {
+      $('.install-log ul').append('<li class="install-log">' + log + '</li>');
+    },
+    
+    sucess: function (log) {
+      $('.install-log ul').append('<li class="install-log log-sucess">' + log + '</li>');
+    },
+    
+    warn: function (log) {
+      $('.install-log ul').append('<li class="install-log log-warn">' + log + '</li>');
+    },
+    
+    error: function (log) {
+      $('.install-log ul').append('<li class="install-log log-error">' + log + '</li>');
+    }
+  };
+
+  $(window).on('load', function () {
+    install.sucess('A página JavaScript principal foi carregada com sucesso!');
+    install.sucess('A página JavaScript das funções gerais foi carregada com sucesso!');
+    console.log('[Instalador] Página carregada! [functions.js]');
+  });
+}(jQuery));
