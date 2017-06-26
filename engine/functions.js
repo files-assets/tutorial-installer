15c4ccc9f9684e73069f6a17a1bac21325788d8b
Luiz Felipe <lffg@users.noreply.github.com>
2017-06-25 21:20:08 -0300

    Atualização.

--- engine/functions.js
+++ engine/functions.js
@@ -7,32 +7,28 @@
  * MIT Licence.
  */
 
-(function ($) {
-  'use strict';
+'use strict';
 
-  var install = {
-    log: function (log) {
-      $('.install-log ul').append('<li class="install-log">' + log + '</li>');
-    },
-    
-    sucess: function (log) {
-      $('.install-log ul').append('<li class="install-log log-sucess">' + log + '</li>');
-    },
-    
-    warn: function (log) {
-      $('.install-log ul').append('<li class="install-log log-warn">' + log + '</li>');
-    },
-    
-    error: function (log) {
-      $('.install-log ul').append('<li class="install-log log-error">' + log + '</li>');
-    }
-  };
+var install = {
+  log: function (log) {
+    $('.install-log ul').append('<li class="install-log">' + log + '</li>');
+  },
+  
+  sucess: function (log) {
+    $('.install-log ul').append('<li class="install-log log-sucess">' + log + '</li>');
+  },
+  
+  warn: function (log) {
+    $('.install-log ul').append('<li class="install-log log-warn">' + log + '</li>');
+  },
+  
+  error: function (log) {
+    $('.install-log ul').append('<li class="install-log log-error">' + log + '</li>');
+  }
+};
 
-  $(window).on('load', function () {
-    install.sucess('A página JavaScript principal foi carregada com sucesso!');
-    install.sucess('A página JavaScript das funções gerais foi carregada com sucesso!');
-    console.log('[Instalador] Página carregada! [functions.js]');
-  });
-
-  return install;
-}(jQuery));
+$(window).on('load', function () {
+  install.sucess('A página JavaScript principal foi carregada com sucesso!');
+  install.sucess('A página JavaScript das funções gerais foi carregada com sucesso!');
+  console.log('[Instalador] Página carregada! [functions.js]');
+});
